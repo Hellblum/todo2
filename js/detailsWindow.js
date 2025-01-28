40cee506c189c22685eb21ca5e46870844ce3fc0
Mykhailo <myscreamo@gmail.com>
2025-01-28 16:52:09 +0100

    try fix description bag

--- js/detailsWindow.js
+++ js/detailsWindow.js
@@ -17,11 +17,22 @@ class DetailsWindow {
 		};
 	}
 	
+	unsavedChanges(task) {
+		if (task.isDescChanges) {
+			this.chosenElements.redSubDescTitle.style.display = 'block';
+			this.chosenElements.subDescTitle.style.display = 'none';
+		} else {
+			this.chosenElements.redSubDescTitle.style.display = 'none';
+			this.chosenElements.subDescTitle.style.display = 'block';
+		}
+	}
+
 	editDescription(task) {
+		this.originalDescription = task.description;
 		this.chosenElements.itemDescription.value = task.description;
-		
-		let tempDescription = task.description;
-		let isDescChanges = false;
+		if (task.isDescChanges === undefined) {
+			task.isDescChanges = false;
+		}
 
 		this.focusBtns =  () => {
 			this.chosenElements.saveDescriptionBtn.style.opacity = '1';
@@ -40,46 +51,33 @@ class DetailsWindow {
 		this.chosenElements.itemDescription.onfocus =  this.focusBtns;
 		this.chosenElements.itemDescription.onblur = this.blurBtns;
 
-		this.chosenElements.itemDescription.oninput = async () => {
-			tempDescription = this.chosenElements.itemDescription.value.trim();
-			if (task.description !== this.chosenElements.itemDescription.value) {
-				isDescChanges = true;
-			} else {
-				isDescChanges = false;
-			}
-			
-			if (isDescChanges) {
-            this.chosenElements.redSubDescTitle.style.display = 'block';
-            this.chosenElements.subDescTitle.style.display = 'none';
-			} else {
-            this.chosenElements.redSubDescTitle.style.display = 'none';
-            this.chosenElements.subDescTitle.style.display = 'block';
-			}
+		this.chosenElements.itemDescription.oninput = () => {
+			const tempDescription = this.chosenElements.itemDescription.value.trim();		
+			task.isDescChanges = tempDescription !== this.originalDescription;
 			task.description = tempDescription;
+			this.unsavedChanges(task);
 		};
 
 		this.chosenElements.saveDescriptionBtn.onclick = async () => {
 			const newDescription = this.chosenElements.itemDescription.value.trim();
 			await this.taskManager.updateTask(task.id, task.title, newDescription, task.completed);
 			task.description = newDescription;
-			tempDescription = newDescription;
-			isDescChanges = false;
-			this.chosenElements.redSubDescTitle.style.display = 'none';
-			this.chosenElements.subDescTitle.style.display = 'block';
+			this.originalDescription = newDescription;
+			task.isDescChanges = false;
+			this.unsavedChanges(task);
 		}
 
 		this.chosenElements.cancelDescriptionBtn.onclick = () => {
-			this.chosenElements.itemDescription.value = task.description;
-			tempDescription = task.description; 
-			isDescChanges = false;
-			this.chosenElements.redSubDescTitle.style.display = 'none';
-			this.chosenElements.subDescTitle.style.display = 'block';
+			this.chosenElements.itemDescription.value = this.originalDescription;
+			task.isDescChanges = false;
+			this.unsavedChanges(task);
 		}
 	}
 	
 	showModal(task) {
 		this.chosenElements.taskDetails.style.display = 'block';
 		document.body.style.overflow = 'hidden';
+		this.unsavedChanges(task);
 		this.editTitle(task);
 		this.editDescription(task);
 	}
